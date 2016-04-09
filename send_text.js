62360902d8442cb600e0b14c27eca49d4581afd8
rikuo hasegawa <rikuo.hase1997@gmail.com>
2016-04-10 02:13:45 +0900

    hoge

--- send_text.js
+++ send_text.js
@@ -3,21 +3,11 @@ var request = require('request');
 
 const proxyUrl = process.env.FIXIE_URL || 	"http://fixie:qO8t5jn6Ahvz6ly@velodrome.usefixie.com:80";
 
-var options = {
-  "url": "https://trialbot-api.line.me/v1/events",
-  "headers": {
-    "Content-type": "application/json; charset=UTF-8",
-    "X-Line-ChannelID": "1462997838",
-    "X-Line-ChannelSecret": "0e6392a115a2d65089479eb5334de457",
-    "X-Line-Trusted-User-With-ACL": "ua9f4a868cf921b7f84075a766320b3ca"
-  },
-  "method": "POST"
-};
+
 
 
 
 exports.handle = function(toUser, text){
-console.log(proxyUrl);
   var body = JSON.stringify({
     "to": [toUser],
     "toChannel": 1383378250,
@@ -29,13 +19,25 @@ console.log(proxyUrl);
       }
   });
 
+  var options = {
+    "url": "https://trialbot-api.line.me/v1/events",
+    "headers": {
+      "Content-type": "application/json; charset=UTF-8",
+      "X-Line-ChannelID": "1462997838",
+      "X-Line-ChannelSecret": "0e6392a115a2d65089479eb5334de457",
+      "X-Line-Trusted-User-With-ACL": "ua9f4a868cf921b7f84075a766320b3ca"
+    },
+    "method": "POST"
+    "body": body
+  };
+
   var proxiedReq = request.defaults({proxy: proxyUrl});
 
-  proxiedReq.post(options, function(err, httpRes, body){
+  proxiedReq.post(options, function(err, httpRes, bodyRes){
     if(err){
       console.log(err);
     }
-    console.log(body);
+    console.log(bodyRes);
   });
 
 };
