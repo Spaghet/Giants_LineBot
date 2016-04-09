214f37b0f977994c6acb1948024ee85f37a5493e
rikuo hasegawa <rikuo.hase1997@gmail.com>
2016-04-09 20:08:20 +0900

    fix?

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function sendRequest(toUser, input){
     },
     method: "POST",
   };
-  var body = qs.stringify({
+  var body = JSON.toString({
     to: [toUser],
     toChannel: channelId,
     eventType: 138311608800106203,
