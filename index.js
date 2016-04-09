be88c00b814be9b604d2f61e5161a0a4b3d06b7f
rikuo hasegawa <rikuo.hase1997@gmail.com>
2016-04-09 16:52:45 +0900

    test callback url

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 var http = require('http');
 
 //Lets define a port we want to listen to
-const PORT=process.env.PORT || 8080;
+const PORT= process.env.PORT || 8080;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
+  if(request.path == '/callback'){
+    console.log('ballin');
+    response.end('successfully hit the callback url');
+  }
     response.end('It Works!! Path Hit: ' + request.url);
 }
 
@@ -15,5 +19,5 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    //console.log("Server listening on: http://localhost:%s", PORT);
 });
